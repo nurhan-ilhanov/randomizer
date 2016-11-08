642ec9d1860e33ce8e4f18dce1eeb776774d98e6
IrenaIvanova <IrenaIvanova@DESKTOP-PC663BL>
2016-11-08 14:48:19 +0200

    adding ElementList and SimpleElement

---
+++ Randomizer.Model/ElementList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Randomizer.Model
+{
+    public class ElementList
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<SimpleElement> List { get; set; }
+    }
+}
